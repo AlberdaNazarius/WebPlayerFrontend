d7bf928670c1ad0670427c56b3dedc165473a87f
AlberdaNazarius <103203230+AlberdaNazarius@users.noreply.github.com>
2023-10-13 12:12:22 +0300

    Added new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SongItemComponent } from './song-item/song-item.component';
+import { PlaylistComponent } from './playlist/playlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SongItemComponent,
+    PlaylistComponent
   ],
   imports: [
     BrowserModule
